753fd714d195dc9e0540dd18ff14e573f0aa9550
gonzalo monja <gonzalormonja1@gmail.com>
2022-09-08 09:31:33 -0300

    programa funcionando

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+
+const KEYS = ['chair', 'height', 'racket', 'touch', 'tunic'];
+
+let first_key = null;
+
+const get_next_key = (keys_used) => {
+    const last_key = keys_used[keys_used.length - 1];
+    let keys_not_used = KEYS.filter(key => !keys_used.includes(key));
+
+    if (keys_not_used.length <= 0) {
+        return { ok: last_key.charAt(last_key.length - 1) == first_key[0], keys: keys_used };
+    } else {
+        const possible_next_keys = keys_not_used.filter(key => last_key.charAt(last_key.length - 1) == key.charAt(0));
+
+        if (possible_next_keys.length <= 0) return { ok: false, keys: [] };
+
+        for (const possible_next_key of possible_next_keys) {
+            return get_next_key([...keys_used, possible_next_key]);
+        }
+    }
+}
+
+
+const test = () => {
+    return new Promise((resolve, reject) => {
+        KEYS.map(key => {
+            first_key = key;
+            const resp = get_next_key([key]);
+            if (resp.ok) {
+                resolve(resp.keys)
+            }
+        })
+        reject()
+    })
+}
+
+test()
+    .then((keys) => {
+        const content_file = keys.reduce((acc, el) => `${acc}${el}\n`, '');
+        fs.writeFile('output.txt', content_file, (err) => {
+            if (err) console.error(err);
+
+            console.log('Archivo escrito correctamente');
+        })
+
+    })
+    .catch(() => console.log('No es posible formar un circulo con las palabras ingresadas'));
